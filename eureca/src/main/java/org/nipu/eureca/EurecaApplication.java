764660f5ab28cff407c2865853a6d6a00b77aacf
ivchu <chuvakhinivan@gmail.com>
2020-02-18 20:52:00 +0300

    Working eureka+feign+ribbon+zuul

---
+++ eureca/src/main/java/org/nipu/eureca/EurecaApplication.java
@@ -0,0 +1,16 @@
+package org.nipu.eureca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurecaApplication.class, args);
+	}
+
+}
